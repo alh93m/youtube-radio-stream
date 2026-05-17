216791b9f57f8575791c2f0769f42b53688e02df
Al <129999699+alh93m@users.noreply.github.com>
2026-05-17 21:28:50 +0300

    Fix ffmpeg command and improve reconnect message

--- radio.sh
+++ radio.sh
@@ -3,13 +3,21 @@
 while true
 do
   ffmpeg -re \
+  -loop 1 -framerate 1 -i bg.png \
   -i "$RADIO_URL" \
-  -loop 1 -i bg.png
-  -c:v libx264 -preset ultrafast -tune stillimage \
+  -map 0:v:0 -map 1:a:0 \
+  -c:v libx264 -preset veryfast -tune stillimage \
   -c:a aac -b:a 128k \
+  -pix_fmt yuv420p \
   -shortest \
+  -fflags +genpts+discardcorrupt \
+  -err_detect ignore_err \
+  -reconnect 1 -reconnect_streamed 1 -reconnect_on_network_error 1 \
+  -reconnect_delay_max 5 \
+  -rw_timeout 15000000 \
   -f flv \
   "$YOUTUBE_URL"
 
+  echo "Stream disconnected... reconnecting in 5s"
   sleep 5
 done
