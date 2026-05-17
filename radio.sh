540ad932e36bbfbc230bb418078308682f578830
Al <129999699+alh93m@users.noreply.github.com>
2026-05-18 01:09:09 +0300

    Update ffmpeg parameters and echo message

--- radio.sh
+++ radio.sh
@@ -4,21 +4,19 @@ while true
 do
   ffmpeg -re \
   -loop 1 -framerate 1 -i bg.jpeg \
+  -user_agent "Mozilla/5.0" \
+  -reconnect 1 -reconnect_streamed 1 -reconnect_on_network_error 1 \
+  -reconnect_delay_max 5 \
   -i "$RADIO_URL" \
   -map 0:v:0 -map 1:a:0 \
-  -vf "scale=4800:2700" \
   -c:v libx264 -preset veryfast -tune stillimage \
+  -vf "scale=1280:720" \
   -c:a aac -b:a 128k \
   -pix_fmt yuv420p \
   -shortest \
-  -fflags +genpts+discardcorrupt \
-  -err_detect ignore_err \
-  -reconnect 1 -reconnect_streamed 1 -reconnect_on_network_error 1 \
-  -reconnect_delay_max 5 \
-  -rw_timeout 15000000 \
   -f flv \
   "$YOUTUBE_URL"
 
-  echo "Stream disconnected... reconnecting in 5s"
+  echo "Stream disconnected... restarting in 5s"
   sleep 5
 done
