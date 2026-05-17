ade9657bfe532c0fbe69529969e9a0f4269319e9
Al <129999699+alh93m@users.noreply.github.com>
2026-05-18 02:01:01 +0300

    Refactor ffmpeg command for radio streaming

--- radio.sh
+++ radio.sh
@@ -3,20 +3,13 @@
 while true
 do
   ffmpeg -re \
-  -loop 1 -framerate 1 -i bg.jpeg \
-  -user_agent "Mozilla/5.0" \
-  -reconnect 1 -reconnect_streamed 1 -reconnect_on_network_error 1 \
-  -reconnect_delay_max 5 \
   -i "$RADIO_URL" \
-  -map 0:v:0 -map 1:a:0 \
-  -c:v libx264 -preset veryfast -tune stillimage \
-  -vf "scale=1280:720" \
+  -f lavfi -i color=c=black:s=1280x720:r=1 \
+  -c:v libx264 -preset ultrafast -tune stillimage \
   -c:a aac -b:a 128k \
-  -pix_fmt yuv420p \
   -shortest \
   -f flv \
   "$YOUTUBE_URL"
 
-  echo "Stream disconnected... restarting in 5s"
   sleep 5
 done
